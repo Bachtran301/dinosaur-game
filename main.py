905d1b2143e8120dbbaaa5dc15e0e9b3469e7763
bachtran <bachtranduy2003@gmail.com>
2024-07-21 23:56:01 +0700

    update main change Enter your name v2.9

--- main.py
+++ main.py
@@ -5,10 +5,81 @@ import random
 import os
 from score_submitter import submit_score, get_high_scores
 
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+
+# Game states
+INITIAL_MENU = "initial_menu"
+NAME_INPUT = "name_input"
+CHARACTER_SELECTION = "character_selection"
+GAME_PLAYING = "game_playing"
+RANKINGS_DISPLAY = "rankings_display"
+game_state = INITIAL_MENU
+
+# Game variables
+game_active = False
+start_time = 0
+score = 0
+game_speed = 6
+high_score = 0
+player_choice = None
+selection_message = ""
+rankings = []
+player_name = ""
+name_input_active = False
+
+# Load images
+sky_surfaces = [pygame.image.load(f'graphics/Sky{i}.png').convert() for i in range(1, 5)]
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+start_image = pygame.image.load('graphics/start_btn.png').convert_alpha()
+exit_image = pygame.image.load('graphics/exit_btn.png').convert_alpha()
+start_image_rect = start_image.get_rect(center=(200, 200))
+exit_image_rect = exit_image.get_rect(center=(600, 200))
+
+# Load player stand images for selection
+player_stand_images = [
+    ('Ninja', pygame.image.load('graphics/player/Ninja/player_stand.png').convert_alpha(), (200, 200), 5, 0),
+    ('Rabbiter', pygame.image.load('graphics/player/Rabbiter/player_stand.png').convert_alpha(), (400, 200), 1.3, 10),
+    ('Villager', pygame.image.load('graphics/player/Villager/player_stand.png').convert_alpha(), (600, 200), 0.8, 20),
+]
+
+for i in range(len(player_stand_images)):
+    player_stand_images[i] = (
+        player_stand_images[i][0],
+        pygame.transform.rotozoom(player_stand_images[i][1], 0, player_stand_images[i][3]),
+        player_stand_images[i][2],
+        player_stand_images[i][4]
+    )
+    player_stand_images[i][1].set_colorkey((0, 0, 0))
+
+# Intro screen
+game_name = test_font.render('Runner', False, (111, 196, 169))
+game_name_rect = game_name.get_rect(center=(400, 80))
+game_message = test_font.render('Press space to run', False, (111, 196, 169))
+game_message_rect = game_message.get_rect(center=(400, 380))
+
+# Groups
+player = pygame.sprite.GroupSingle()
+obstacle_group = pygame.sprite.Group()
+coin_group = pygame.sprite.Group()
+
+# Timer
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 1500)
+coin_timer = pygame.USEREVENT + 2
+pygame.time.set_timer(coin_timer, 5000)
+min_obstacle_timer = 800
+max_obstacle_timer = 1500
+
+# Load high scores
+high_scores = get_high_scores()
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, character):
         super().__init__()
-        # nhân vật
         self.character = character
         if character == 'Ninja':
             self.load_Ninja()
@@ -27,9 +98,11 @@ class Player(pygame.sprite.Sprite):
     def load_Ninja(self):
         self.player_walk = [self.load_and_scale(f'graphics/player/Ninja/{i}.png', 3) for i in range(8)]
         self.player_jump = self.load_and_scale('graphics/player/Ninja/jump.png', 3)
+    
     def load_2(self):
         self.player_walk = [self.load_and_scale(f'graphics/player/2/{i}.png', 3) for i in range(8)]
         self.player_jump = self.load_and_scale('graphics/player/2/jump.png', 3)
+    
     def load_Rabbiter(self):
         self.player_walk = [self.load_and_scale(f'graphics/player/Rabbiter/{i}.png', 1) for i in range(3)]
         self.player_jump = self.load_and_scale('graphics/player/Rabbiter/jump.png', 1)
@@ -130,8 +203,7 @@ class Coin(pygame.sprite.Sprite):
         self.rect.x -= 6
         self.destroy()
 
-    def destroy(self):
-        if self.rect.x <= -100:
+    def destroy(self): 
             self.kill()
 
 def display_score():
@@ -170,12 +242,6 @@ def display_rankings(rankings):
         score_rect = score_surf.get_rect(center=(600, 150 + i * 50))
         screen.blit(score_surf, score_rect)
 
-def collisions(player, obstacles):
-    if obstacles:
-        for obstacle_rect in obstacles:
-            if player.colliderect(obstacle_rect): return False
-    return True
-
 def collision_sprite():
     if pygame.sprite.spritecollide(player.sprite, obstacle_group, False):
         obstacle_group.empty()
@@ -191,7 +257,6 @@ def save_high_scores(scores, filename='high_scores.txt'):
     with open(filename, 'w') as file:
         for score in scores:
             file.write(f"{score}\n")
-
 
 def load_high_scores(filename='high_scores.txt'):
     if not os.path.exists(filename):
@@ -210,93 +275,13 @@ def handle_name_input(event):
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_RETURN:
             if player_name:
-                game_state = INITIAL_MENU
+                game_state = CHARACTER_SELECTION
                 name_input_active = False
         elif event.key == pygame.K_BACKSPACE:
             player_name = player_name[:-1]
         else:
-            if len(player_name) < 10:  # Giới hạn độ dài tên
+            if len(player_name) < 10 and event.unicode.isprintable():  # Giới hạn độ dài tên và chỉ cho phép ký tự in được
                 player_name += event.unicode
-            
-pygame.init()
-screen = pygame.display.set_mode((800, 400))
-pygame.display.set_caption('Runner')
-clock = pygame.time.Clock()
-test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
-game_active = False
-start_time = 0
-score = 0
-game_speed = 6
-high_score = 0
-player_choice = None
-selection_message = ""
-rankings = []
-
-
-# Load all player stand images for selection
-player_stand_images = [
-    ('Ninja', pygame.image.load('graphics/player/Ninja/player_stand.png').convert_alpha(), (200, 200), 5, 0),
-    ('Rabbiter', pygame.image.load('graphics/player/Rabbiter/player_stand.png').convert_alpha(), (400, 200), 1.3, 10),
-    ('Villager', pygame.image.load('graphics/player/Villager/player_stand.png').convert_alpha(), (600, 200), 0.8, 20),
-]
-
-for i in range(len(player_stand_images)):
-    player_stand_images[i] = (
-        player_stand_images[i][0],
-        pygame.transform.rotozoom(player_stand_images[i][1], 0, player_stand_images[i][3]),
-        player_stand_images[i][2],
-        player_stand_images[i][4]
-    )
-    player_stand_images[i][1].set_colorkey((0, 0, 0))
-
-
-
-sky_surfaces = [
-    pygame.image.load(f'graphics/Sky{i}.png').convert() for i in range(1, 5)
-]
-ground_surface = pygame.image.load('graphics/ground.png').convert()
-
-# Intro screen
-game_name = test_font.render('Runner', False, (111, 196, 169))
-game_name_rect = game_name.get_rect(center=(400, 80))
-
-game_message = test_font.render('Press space to run', False, (111, 196, 169))
-game_message_rect = game_message.get_rect(center=(400, 380))
-
-start_image = pygame.image.load('graphics/start_btn.png').convert_alpha()
-exit_image = pygame.image.load('graphics/exit_btn.png').convert_alpha()
-start_image_rect = start_image.get_rect(center=(200, 200))
-exit_image_rect = exit_image.get_rect(center=(600, 200))
-
-# Groups
-player = pygame.sprite.GroupSingle()
-obstacle_group = pygame.sprite.Group()
-coin_group = pygame.sprite.Group()
-
-# Timer
-obstacle_timer = pygame.USEREVENT + 1
-pygame.time.set_timer(obstacle_timer, 1500)
-
-coin_timer = pygame.USEREVENT + 2
-pygame.time.set_timer(coin_timer, 5000)
-
-min_obstacle_timer = 800  # Thời gian tối thiểu (mili giây)
-max_obstacle_timer = 1500  # Thời gian tối đa (mili giây)
-
-# Load high scores
-high_scores = load_high_scores()
-
-
-# Game states
-INITIAL_MENU = "initial_menu"
-CHARACTER_SELECTION = "character_selection"
-GAME_PLAYING = "game_playing"
-RANKINGS_DISPLAY = "rankings_display"
-game_state = INITIAL_MENU
-NAME_INPUT = "name_input"
-game_state = NAME_INPUT  # Bắt đầu với trạng thái nhập tên
-player_name = ""
-name_input_active = True
 
 while True:
     for event in pygame.event.get():
@@ -304,33 +289,28 @@ while True:
             save_high_scores(high_scores)
             pygame.quit()
             exit()
-        if game_state == NAME_INPUT:
-            handle_name_input(event)
-        elif game_state == GAME_PLAYING:
-            if event.type == obstacle_timer:
-                obstacle_group.add(Obstacle(choice(['fly', 'spikes', 'snail', 'bee', 'worm', 'tooth'])))
-                # Điều chỉnh timer dựa trên điểm số
-                new_timer = max(min_obstacle_timer, max_obstacle_timer - (score * 15))  # Giảm khoảng thời gian khi điểm số tăng
-                pygame.time.set_timer(obstacle_timer, new_timer)
-            if event.type == coin_timer:
-                if random.random() < 0.05:  # 5% cơ hội tạo ra kim cương
-                    coin_group.add(Coin('diamond'))
-                else:
-                    coin_group.add(Coin('gold'))
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if player.rect.collidepoint(event.pos) and player.rect.bottom >= 300:
-                    player.gravity = -20
-
+        
+        if game_state == INITIAL_MENU:
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and player.sprite.rect.bottom >= 300:
-                    player.sprite.gravity = -20
+                if event.key == pygame.K_s:
+                    game_state = NAME_INPUT
+                elif event.key == pygame.K_e:
+                    pygame.quit()
+                    exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if start_image_rect.collidepoint(event.pos):
+                    game_state = NAME_INPUT
+                elif exit_image_rect.collidepoint(event.pos):
+                    pygame.quit()
+                    exit()
+        elif game_state == NAME_INPUT:
+            handle_name_input(event)
         elif game_state == CHARACTER_SELECTION:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 if player_choice:
                     game_state = GAME_PLAYING
                     start_time = int(pygame.time.get_ticks() / 1000)
                     player.add(Player(player_choice))
-
             if event.type == pygame.MOUSEBUTTONDOWN:
                 for name, img, pos, required_score in player_stand_images:
                     if img.get_rect(center=pos).collidepoint(event.pos):
@@ -339,32 +319,65 @@ while True:
                             selection_message = ""
                         else:
                             selection_message = f"{name} requires {required_score} points!"
-        elif game_state == INITIAL_MENU:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_s:
-                    game_state = CHARACTER_SELECTION
-                elif event.key == pygame.K_e:
-                    pygame.quit()
-                    exit()
+        elif game_state == GAME_PLAYING:
+            if event.type == obstacle_timer:
+                obstacle_group.add(Obstacle(choice(['fly', 'spikes', 'snail', 'bee', 'worm', 'tooth'])))
+                new_timer = max(min_obstacle_timer, max_obstacle_timer - (score * 15))
+                pygame.time.set_timer(obstacle_timer, new_timer)
+            if event.type == coin_timer:
+                if random.random() < 0.05:
+                    coin_group.add(Coin('diamond'))
+                else:
+                    coin_group.add(Coin('gold'))
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if start_image_rect.collidepoint(event.pos):
-                    game_state = CHARACTER_SELECTION
-                elif exit_image_rect.collidepoint(event.pos):
-                    pygame.quit()
-                    exit()
+                if player.sprite.rect.collidepoint(event.pos) and player.sprite.rect.bottom >= 300:
+                    player.sprite.gravity = -20
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player.sprite.rect.bottom >= 300:
+                    player.sprite.gravity = -20
         elif game_state == RANKINGS_DISPLAY:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_state = CHARACTER_SELECTION
-    if game_state == NAME_INPUT:
-        screen.fill((94, 129, 162))
-        name_prompt = test_font.render('Enter your name:', False, (255, 255, 255))
-        screen.blit(name_prompt, (300, 150))
-        name_surface = test_font.render(player_name, False, (255, 255, 255))
-        screen.blit(name_surface, (300, 200))
-        if pygame.time.get_ticks() % 1000 < 500:  # Nhấp nháy con trỏ
-            pygame.draw.line(screen, (255, 255, 255), (300 + name_surface.get_width(), 200),(300 + name_surface.get_width(), 240), 2)
-        
 
+    if game_state == INITIAL_MENU:
+        screen.fill((94, 129, 162))
+        screen.blit(start_image, start_image_rect)
+        screen.blit(exit_image, exit_image_rect)
+    elif game_state == NAME_INPUT:
+        screen.fill((94, 129, 162))
+        name_prompt = test_font.render('Enter your name: ', False, (255, 255, 255))
+        name_surface = test_font.render(player_name, False, (255, 255, 255))
+    
+        prompt_width = name_prompt.get_width()
+        total_width = prompt_width + name_surface.get_width()
+        start_x = (800 - total_width) // 2  # Căn giữa theo chiều ngang
+    
+        screen.blit(name_prompt, (start_x, 150))
+        screen.blit(name_surface, (start_x + prompt_width, 150))
+    
+        if pygame.time.get_ticks() % 1000 < 500:  # Nhấp nháy con trỏ
+            cursor_x = start_x + prompt_width + name_surface.get_width()
+            pygame.draw.line(screen, (255, 255, 255), (cursor_x, 150), (cursor_x, 175), 2)
+    elif game_state == CHARACTER_SELECTION:
+        screen.fill((94, 129, 162))
+        screen.blit(game_name, game_name_rect)
+        screen.blit(game_message, game_message_rect)
+        for _, img, pos, _ in player_stand_images:
+            screen.blit(img, img.get_rect(center=pos))
+
+        score_message = test_font.render(f'High score: {high_score}', False, (111, 196, 169))
+        score_message_rect = score_message.get_rect(center=(400, 120))
+        screen.blit(score_message, score_message_rect)
+
+        if player_choice and selection_message == "":
+            selected_surf = test_font.render(f'Selected: {player_choice}', False, (111, 196, 169))
+            selected_rect = selected_surf.get_rect(center=(400, 300))
+            screen.blit(selected_surf, selected_rect)
+
+        if selection_message:
+            message_surf = test_font.render(selection_message, False, (255, 0, 0))
+            message_rect = message_surf.get_rect(center=(400, 320))
+            screen.blit(message_surf, message_rect)
     elif game_state == GAME_PLAYING:
         screen.blit(sky_surfaces[min(score // 10, 3)], (0, 0))
         screen.blit(ground_surface, (0, 300))
@@ -391,54 +404,15 @@ while True:
             rankings.sort(key=lambda x: x["score"], reverse=True)
             if len(rankings) > 5:
                 rankings = rankings[:5]
-            submit_score(player_name, score)  # Thay "Player" bằng tên người chơi thực tế
+            submit_score(player_name, score)
             high_scores = get_high_scores()
-            # Cập nhật rankings từ high_scores
             rankings = high_scores
-    elif game_state == CHARACTER_SELECTION:
-        if score > high_score:
-            high_score = score
-        
-        screen.fill((94, 129, 162))
-        screen.blit(game_name, game_name_rect)
-        screen.blit(game_message, game_message_rect)
-        for _, img, pos, _ in player_stand_images:
-            screen.blit(img, img.get_rect(center=pos))
-
-        score_message = test_font.render(f'High score: {high_score}', False, (111, 196, 169))
-        score_message_rect = score_message.get_rect(center=(400, 120))
-        screen.blit(game_name, game_name_rect)
-
-        if score == 0:
-            screen.blit(game_message, game_message_rect)
-        else:
-            screen.blit(score_message, score_message_rect)
-        if player_choice and selection_message == "":
-            selected_surf = test_font.render(f'Selected: {player_choice}', False, (111, 196, 169))
-            selected_rect = selected_surf.get_rect(center=(400, 300))
-            screen.blit(selected_surf, selected_rect)
-
-        if selection_message:
-            message_surf = test_font.render(selection_message, False, (255, 0, 0))
-            message_rect = message_surf.get_rect(center=(400, 320))
-            screen.blit(message_surf, message_rect)
-
-    elif game_state == INITIAL_MENU:
-        screen.fill((94, 129, 162))
-        screen.blit(start_image, start_image_rect)
-        screen.blit(exit_image, exit_image_rect)
-    
     elif game_state == RANKINGS_DISPLAY:
         screen.fill((94, 129, 162))
         add_score_to_rankings(score)
-        high_scores = get_high_scores()  # Cập nhật danh sách điểm cao sau khi thêm điểm mới
+        high_scores = get_high_scores()
         display_high_score(max(score['score'] for score in high_scores) if high_scores else 0)
         display_rankings(high_scores)
-        #display_rankings(rankings)
-        #rank_message = test_font.render('Press SPACE to go to character selection', False, (111, 196, 169))
-        #rank_message_rect = rank_message.get_rect(center=(400, 350))
-        #screen.blit(rank_message, rank_message_rect)
 
     pygame.display.update()
     clock.tick(60)
-###123
