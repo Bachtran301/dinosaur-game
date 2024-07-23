eee452605b0549536ad84966e347a44412b62954
bachtran <bachtranduy2003@gmail.com>
2024-07-24 00:57:13 +0700

    update v1.1

--- app.py
+++ app.py
@@ -52,4 +52,6 @@ def init_db():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+    #app.run(debug=True, host='0.0.0.0', port=5000)
