c7d42c0a234e1e0823f286b1ca1d59d9eca8bb66
bachtran <bachtranduy2003@gmail.com>
2024-07-24 13:59:03 +0700

    config

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
+    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/your_database_name'
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
